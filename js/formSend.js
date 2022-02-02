8826e6264fb0d62736808f09b12529af5a7fe84e
loboykois <loboykois@gmail.com>
2022-02-03 00:23:56 +0200

    rewrite with classes

--- js/formSend.js
+++ js/formSend.js
@@ -1,53 +1,92 @@
 "use strict";
 
-const getBackBtn = document.querySelector("button.form__btn--back");
+// formToSend.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   const nameInput = document.getElementById("name").value.trim();
+//   const surnameInput = document.getElementById("surname").value.trim();
+//   const weightInput = document.getElementById("weight").value.trim();
+//   const heightInput = document.getElementById("height").value.trim();
+//   const trainedInput = document.getElementById("trained").value.trim();
 
-const saveBtn = document.querySelector("button.form__btn--save");
+//   if (
+//     !nameInput ||
+//     !surnameInput ||
+//     !weightInput ||
+//     !heightInput ||
+//     !trainedInput
+//   ) {
+//     return;
+//   }
 
-getBackBtn.addEventListener("click", backToFlowOne);
+//   const person = {
+//     name: nameInput,
+//     surname: surnameInput,
+//     weight: weightInput + " KG",
+//     height: heightInput + " CM",
+//     trained: trainedInput + " Times",
+//   };
 
-const formToSend = document.getElementById("myForm");
-const formFields = formToSend.elements;
+//   localStorage.setItem("person", JSON.stringify(person));
 
-function backToFlowOne() {
-  window.location.href = "frontend-task.html";
+//   window.location.href = "frontend-task.html";
+// });
+
+// ======================= Class person =======================
+
+class Person {
+  constructor(descriptor) {
+    this.id = descriptor.id;
+    this.name = descriptor.name;
+    this.surname = descriptor.surname;
+    this.weight = descriptor.weight;
+    this.height = descriptor.height;
+    this.trained = descriptor.trained;
+  }
 }
 
-formToSend.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const nameInput = document.getElementById("name").value.trim();
-  const surnameInput = document.getElementById("surname").value.trim();
-  const weightInput = document.getElementById("weight").value.trim();
-  const heightInput = document.getElementById("height").value.trim();
-  const trainedInput = document.getElementById("trained").value.trim();
+// ======================= Trainee =======================
 
-  if (
-    !nameInput ||
-    !surnameInput ||
-    !weightInput ||
-    !heightInput ||
-    !trainedInput
-  ) {
-    return;
-  }
-
-  const person = {
-    name: nameInput,
-    surname: surnameInput,
-    weight: weightInput + " KG",
-    height: heightInput + " CM",
-    trained: trainedInput + " Times",
-  };
-
-  localStorage.setItem("person", JSON.stringify(person));
-
-  window.location.href = "frontend-task.html";
+const trainee = new Person({
+  id: Date.now(),
+  name: document.getElementById("name").value.trim(),
+  surname: document.getElementById("surname").value.trim(),
+  weight: document.getElementById("weight").value.trim(),
+  height: document.getElementById("height").value.trim(),
+  trained: document.getElementById("trained").value.trim(),
 });
 
-// for (let i = 0; i < formFields.length; i++) {
-//   let field = formFields[i];
+// ======================= Storage controller =======================
 
-//   saveBtn.addEventListener("click", () => {
-//     localStorage.setItem(field.name, field.value);
-//   });
-// }
+class StorageController {
+  saveToStorage() {
+    localStorage.setItem();
+  }
+}
+
+// ======================= BTN controller =======================
+
+const getBackBtn = document.querySelector("button.form__btn--back");
+const saveBtn = document.querySelector("button.form__btn--save");
+
+class BtnController {
+  getBack() {
+    getBackBtn.addEventListener("click", () => {
+      window.location.href = "frontend-task.html";
+    });
+  }
+  saveToStorage() {
+    saveBtn.addEventListener("submit", (event) => {
+      event.preventDefault();
+      // localStorage.setItem("trainee", JSON.stringify(trainee));
+    });
+  }
+}
+
+const buttons = new BtnController();
+
+buttons.getBack();
+// buttons.saveToStorage();
+
+saveBtn.addEventListener("submit", (e) =>{
+   e.preventDefault()
+})
