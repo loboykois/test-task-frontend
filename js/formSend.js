c592869bf403becde27c722a03ee18cd132e0380
loboykois <loboykois@gmail.com>
2022-02-05 00:24:21 +0200

    finaly working

--- js/formSend.js
+++ js/formSend.js
@@ -1,70 +1,65 @@
 "use strict";
+// ==================== UUID ====================
 
-// ======================= Class person =======================
-class Person {
-//   constructor(descriptor) {
-//     this.id = descriptor.id;
-//     this.name = descriptor.name;
-//     this.surname = descriptor.surname;
-//     this.weight = descriptor.weight;
-//     this.height = descriptor.height;
-//     this.trained = descriptor.trained;
-//   }
-  id= Date.now();
-  name= document.getElementById("name").value.trim();
-  surname = document.getElementById("surname").value.trim();
-  weight = document.getElementById("weight").value.trim();
-  height = document.getElementById("height").value.trim();
-  trained = document.getElementById("trained").value.trim();
+function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
 }
 
-// ======================= Trainee =======================
+// ==================== Form ====================
 
+const myForm = document.querySelector("form");
+const formName = document.getElementById("name");
+const formSurname = document.getElementById("surname");
+const formWeight = document.getElementById("weight");
+const formHeight = document.getElementById("height");
+const formTrained = document.getElementById("trained");
+const backBtn = document.querySelector(".form__btn--back");
+const saveBtn = document.querySelector(".form__btn--save");
 
-// const trainee = new Person({
-//   id: Date.now(),
-//   name: document.getElementById("name").value.trim(),
-//   surname: document.getElementById("surname").value.trim(),
-//   weight: document.getElementById("weight").value.trim(),
-//   height: document.getElementById("height").value.trim(),
-//   trained: document.getElementById("trained").value.trim(),
-// });
-
-// ======================= Storage controller =======================
+// ==================== Storage controller ====================
 
 class StorageController {
-  saveToStorage() {
-   let counter = 1;
-    localStorage.setItem("trainee" + counter, JSON.stringify(Person));
-    counter++
-  }
+   addItem(obj) {
+     localStorage.setItem("person" + " " + obj.name, JSON.stringify(obj));
+   }
+
+   remove(obj) {
+      localStorage.removeItem("person" + " " + obj.name)
+   }
+ 
+   getItem(obj) {
+     localStorage.getItem("person" + " " + obj.name, JSON.parse(obj));
+   }
+ 
+   clear() {
+     localStorage.clear();
+   }
+ }
+ 
+ const storage = new StorageController();
+
+
+// ==================== Create person ====================
+
+function createPerson(event) {
+  event.preventDefault();
+
+  const person = {
+    id: uuidv4(),
+    name: formName.value,
+    surname: formSurname.value,
+    weight: formHeight.value,
+    height: formHeight.value,
+    trained: formTrained.value,
+  };
+
+  //   storageControl(person);
+  storage.addItem(person);
 }
 
-// ======================= Storage =======================
+myForm.addEventListener("submit", createPerson);
 
-const storage = new StorageController();
-
-// ======================= BTN controller =======================
-
-const getBackBtn = document.querySelector("button.form__btn--back");
-const saveBtn = document.querySelector("button.form__btn--save");
-
-class BtnController {
-  getBack() {
-    getBackBtn.addEventListener("click", () => {
-      window.location.href = "frontend-task.html";
-    });
-  }
-  saveToStorage() {
-    saveBtn.addEventListener("click", (event) => {
-      event.preventDefault();
-      storage.saveToStorage();
-      // localStorage.setItem("trainee", JSON.stringify(trainee));
-    });
-  }
-}
-
-const buttons = new BtnController();
-
-buttons.getBack();
-buttons.saveToStorage();
